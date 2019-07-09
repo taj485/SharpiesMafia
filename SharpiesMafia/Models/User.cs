b968ff633063b73e7d0e067ae56e668ca0238163
Taj <tajbi21@gmail.com>
2019-07-09 15:08:07 +0100

    Created User class under table

---
+++ SharpiesMafia/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SharpiesMafia.Models
+{
+    public class User
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public string connection_id { get; set; }
+        public long game_id { get; set; }
+        public string role { get; set; }
+        public bool is_dead { get; set; }
+    }
+}
