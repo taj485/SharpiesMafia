3be4b6ffbd07248ec19bddfe98798a917f9115ba
Taj <tajbi21@gmail.com>
2019-07-09 10:52:18 +0100

    setting up MVC scaffold

---
+++ SharpiesMafia/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SharpiesMafia.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
