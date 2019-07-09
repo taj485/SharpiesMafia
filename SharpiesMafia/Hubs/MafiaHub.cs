f2fbdfce3bccd1aa14111868ff93f78f79ca4ca8
edwardreeve <edreeve+git@gmail.com>
2019-07-09 11:07:53 +0100

    Created Signal R Hub

---
+++ SharpiesMafia/Hubs/MafiaHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SharpiesMafia.Hubs
+{
+    public class MafiaHub : Hub
+    {
+        //This was the example method from the chatroom article example
+        //public async Task SendMessage(string user, string message)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //}
+    }
+}
