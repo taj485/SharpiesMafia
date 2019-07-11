6ae03e4b97a68d6b819e67394e9f0e6a83be424d
Kelvin Samuel <kelvin@kks110.com>
2019-07-11 14:44:37 +0100

    Starting working on the start game partial

--- SharpiesMafia/Controllers/HomeController.cs
+++ SharpiesMafia/Controllers/HomeController.cs
@@ -22,5 +22,10 @@ namespace SharpiesMafia.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        public IActionResult StartGameScreen()
+        {
+            return PartialView("_StartGamePartial.cshtml");
+        }
+
     }
 }
